25d6435c9d6182eb8c02759251323109a8b5328d
Sa-mmie <samuelquaye21106@gmail.com>
2026-05-31 04:34:46 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+#File: initials.py
+#Description: Printing out of my initials.
+#Assignment Number: 2
+#
+#Name: Samuel Nii Ayikwei Quaye.
+#STUDENT ID: 2425404768
+#Email: 2425404768@live.gctu.edu.gh
+#Grader: Augustus
+#
+#On my honor, Samuel Quaye, this programming assignment is my own work
+#and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    #Print initials SNQ in styled large block letters with the use of S, N, Q characters.
 
+    print()
+    print('...SNQ')
+    print()
+    print('...SSSSSSSSSSSS........NN........NN........QQQQQQQQQQQQ.....')
+    print('...SS..................NNN.......NN........QQ........QQ.....')
+    print('...SS..................NNNN......NN........QQ........QQ.....')
+    print('...SS..................NN.NN.....NN........QQ........QQ.....')
+    print('...SSSSSSSSSSSS........NN..NN....NN........QQ........QQ.....')
+    print('.............SS........NN...NN...NN........QQ........QQ.....')
+    print('.............SS........NN....NN..NN........QQ...QQ...QQ.....')
+    print('.............SS........NN.....NNNNN........QQ.....QQ..Q.....')
+    print('.............SS...**...NN......NNNN...**...QQQQQQQQQQQQ.....')
+    print('...SSSSSSSSSSSS...**...NN.......NNN...**.............QQ.....')
+    print()
 
 
 main()
